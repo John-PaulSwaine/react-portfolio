2669f6e3507e7d11496527353a610b0b1ca27a49
John-Paul Swaine <179924473+John-PaulSwaine@users.noreply.github.com>
2025-07-13 14:07:37 +0100

    added functionality to go from first half straight to second half and vice versa.

--- src/pages/week1to6.jsx
+++ src/pages/week1to6.jsx
@@ -24,27 +24,33 @@ const Week1to6 = () => {
                 <br /><br />
                 <h3>Keycode Generator</h3>
                 For the keycode assignment, I kept it simple both code-wise and aesthetically. This was done so as not to affect people who may suffer with photo-sensitive epilepsy by having different colours flashing for different keys.
+                <br />
                 <a href="https://john-paulswaine.github.io/Keycode-Assignment/"><button>Keycode Generator</button></a>
                 <br /><br />
                 <h3>Drumkit</h3>
                 For this assignment I decided to title my drumkit "Drummer Hero" taking from the popular game of guitar hero for my main inspiration.
+                <br />
                 <a href="https://john-paulswaine.github.io/Drumkit-Assignment/"><button>Drummer Hero</button></a>
                 <br /><br />
                 <h3>Dice Game</h3>
                 For this assignment I had to create a dice game with a 1-player and 2-player game modes. I found this enjoyable as it enabled me to learn how to hide images until a certain outcome.
+                <br />
                 <a href="https://john-paulswaine.github.io/Dice-Game/"><button>Dice Game</button></a>
                 <br /><br />
                 <h3>Simon Says</h3>
                 For this I had to create a game that selected random boxes and the player had to repeat in sequence with each level growing increasingly difficult. For this I also got assistance in implementing a 3-life rule where to lose you had to lose all 3 lives.
+                <br />
                 <a href=""><button>Simon Says</button></a>
                 <br /><br />
                 <h3>Number Guesser</h3>
                 For this I had to generate a game where the computer generates a number between 1-100 and the user has to guess. It also had to advise higher or lower and count the amount of guesses made.
+                <br />
                 <a href="https://john-paulswaine.github.io/Simon-Says/"><button>Guess the Number</button></a>
                 </p>
             <br />
             <h1>Week 4</h1>
                 <p>This week we had to work as part of a group in order to create a cyber pet game in which the pet has stat bars that continually decrease until death of the pet. It also had to include multiple buttons (e.g. eat, drink, sleep) to increase and decrease stat bars depending on which button was pressed (e.g. if the eat button was pressed the fed/hunger bar would increase while the thirst bar decreased). For this assignment I mainly focused on the classes for each of the pet options, provided assistance where needed to other team mates as well as bringing the page all together with the work of each team mate. I felt it was important to bring everything together to aid in the readability and to ensure functionality.</p>
+                <br />
                 <a href="https://john-paulswaine.github.io/Cyberpet/"><button>Beast Masters</button></a>
             <br />
             <h1>Week 5</h1>
@@ -52,18 +58,21 @@ const Week1to6 = () => {
                     <br /><br />
                     <h3>Calculator</h3>
                     For this task, I had to create a calculator app which was fully functioning utilising React. This had to be done using various methods and data types such as mapping arrays, utilisation of the evaluate method from the mathjs library and having buttons to be able to do various sums while also displaying the input.
-                    <a href=""><button>Calculator</button></a>
+                    <br />
+                    <a href="https://john-pauls-calculator.vercel.app/"><button>Calculator</button></a>
                     <br /><br />
                     <h3>To-Do List</h3>
                     My challenge for this assignment activity was to create a to-do list utilising React. This had to include the ability to add items to the list while also deleting them, allow the user to delete items as well as mark items as completed. This also had to include the usage of the state value as well as receiving user input.
-                    <a href=""><button>To-Do List</button></a>
+                    <br />
+                    <a href="https://john-pauls-to-do-list.vercel.app/"><button>To-Do List</button></a>
                 <br />
                 <h1>Week 6</h1>
                     <p>During week 6 we had to create a full front-end web app using everything learnt so far in order to solidify and recap everything covered. The challenge for this assignment was to, as a group, create a React app for an e-commerce site for cats utilising the cat api for images as well as utilising fakerjs. For this assignment, I took on more of a leading role within the team while ensuring that each team member was working on what suited their strengths while also utilising an agile methodology within the team. I felt that my leadership was a great help to others within the team as I ensured everything was organised and that people were not working on the same thing when they weren't meant to be.</p>
-                    <a href=""><button>Cats4Lyf</button></a>
+                    <br />
+                    <a href="https://cats4lyf-john-paul-swaines-projects.vercel.app/"><button>Cats4Lyf</button></a>
                 <br />
-                
                 </p>
+                <a href="week-7-to-12"><button>Weeks 7-12</button></a>
         </div>
     )
 }
--- src/pages/week7to12.jsx
+++ src/pages/week7to12.jsx
@@ -3,7 +3,22 @@ import "../App.css";
 const Week7to12 = () => {
     return (
         <div>
-            
+            <h1>Week 7</h1>
+            <p></p>
+            <br />
+            <h1>Week 8</h1>
+            <p></p>
+            <br />
+            <h1>Week 9</h1>
+            <p></p>
+            <br />
+            <h1>Week 10</h1>
+            <p></p>
+            <br />
+            <h1>Final Project</h1>
+            <p></p>
+            <br />
+            <a href="week-1-to-6"><button>Weeks 1-6</button></a>
         </div>
     )
 }
