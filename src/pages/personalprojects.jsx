57af9c380c6e0de42504880dfddc4823e1efa9d7
John-Paul Swaine <179924473+John-PaulSwaine@users.noreply.github.com>
2025-07-13 14:26:37 +0100

    completed personal projects page

--- src/pages/personalprojects.jsx
+++ src/pages/personalprojects.jsx
@@ -1,7 +1,14 @@
 const PersonalProjects = () =>{
     return (
         <div>
-            <p>this page will contain any and all personal projects such as my quiz.</p>
+            <h1>Personal Projects</h1>
+            <p>This section of my portfolio will include any and all projects undertaken solely by myself, including the formation of the idea(s) leading to the projects.</p>
+            <br />
+            <h2>Quiz</h2>
+            <p>This project began whilst I was still learning the basics of Javascript, HTML and CSS. The purpose was to enhance my skills further. I later proceeded to transform this project into a new REACT project to further enhance my skills across the front end. This quiz has since continued to grow and will continue to do so over time. However, the basic version of the quiz will remain in it's original form, leaving only the REACT version to be updated moving forward.</p>
+            <a href="https://john-paulswaine.github.io/Self-Developed-Quiz/"><button>Basic Quiz</button></a>
+            <br /><br />
+            <a href="https://john-paul-swaine-react-quiz.netlify.app/"><button>REACT Quiz</button></a>
         </div>
     )
 }
