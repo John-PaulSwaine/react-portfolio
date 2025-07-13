5e620c1dde222fe79be8af972679658ba95fd870
John-Paul Swaine <179924473+John-PaulSwaine@users.noreply.github.com>
2025-07-13 15:09:41 +0100

    finished about section

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -9,11 +9,14 @@ const About = () => {
             <h3>My coding journey</h3>
             <p>I began my coding journey in 2024 when I began a course with CodeNation. This course included learning HTML, CSS, JavaScript, REACT, RESTful APIs, SQL and Non-SQL DataBases and culminated in a Full Stack project. </p>
             <br />
-            <h3>A bit about my background</h3>
-            <p>Here I will put a bit of background info into who I am as a person.</p>
-            <br />
-            <h3>Why coding?</h3>
-            <p>Here I will put a bit of information into why I chose to get into coding and why I wish to pursue it further.</p>
+            <h3>A bit about me</h3>
+            <p>I’m from Liverpool and grew up in care, an experience that taught me resilience and emotional maturity from an early age. Throughout my education, I balanced academic and creative pursuits — achieving strong GCSEs including an A in Maths and B in English, alongside BTEC qualifications in science, performing arts, and acting. More recently, I have pursued my passion for software development, focusing on web development technologies like React, JavaScript, and HTML.
+            <br /><br />
+            Although my formal education specialized in acting, I have been deeply drawn to coding since day one and am excited to build my career in this dynamic field. One project I’m particularly proud of is a quiz I developed to deepen my knowledge and help others learn. I bring a strong attention to detail, a dedicated work ethic, and a continuous drive to improve myself to every project I take on.
+            <br /><br />
+            Creativity, precision, and collaboration guide my work, though I’m equally comfortable working independently. Outside of coding, I enjoy gaming, anime, football, and spending time with family.
+            <br /><br />
+            I’m currently seeking full-time opportunities in software development but am open to part-time roles as well. Ultimately, my long-term goal is to live and work in Japan, combining my passion for technology with my love for new cultures.</p>
         </div>
     )
 }
